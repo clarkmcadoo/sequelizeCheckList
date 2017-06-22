859b9dedb29b88a75ba03f2caef2af2a6cb080bf
clarkmcadoo <clark.mcadoo@gmail.com>
2017-06-21 20:45:16 -0500

    init

---
+++ main.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const mustacheExpress = require("mustache-express");
+const port = 3000;
+const bodyParser = require("body-parser");
+const expressValidator = require("express-validator")
+var todo = [];
+var complete = [];
+const app = express();
+
+app.engine("mustache", mustacheExpress());
+app.set("views", "./public");
+app.set("view engine", "mustache");
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(expressValidator());
+
+app.use("/", express.static("./public"));
+
+
+app.get("/", function(require, response){
+    response.render("index", {todo: todo});
+})
+
+app.post("/", function(require, response){
+    var newTodo = require.body.todo;
+    todo.push(newTodo);
+    response.redirect("/");
+
+})
+
+app.listen(port, function(){
+    console.log("You are running the check-list app on port:", port);
+});
